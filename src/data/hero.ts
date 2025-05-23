ec3a43528e628885d7e5ee3cef72d1526fb2e828
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-23 11:24:53 -0600

    imagen

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
     heading: 'Impulsamos tu operación con calidad energética.',
     subheading: 'En MGO Industrial, somos especialistas en la distribución al por mayor de petróleo y productos petroleros, con más de una década de experiencia ofreciendo soluciones confiables y eficientes en México.',
-    centerImageSrc: '/images/hero-mockup.png',
+    centerImageSrc: '/images/hero.png',
 }
