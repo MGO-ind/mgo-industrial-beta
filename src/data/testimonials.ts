b22c2fa341a0ed78850538bf7755ad87bd72c055
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-05-29 17:19:31 -0600

    mision vision

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -3,21 +3,15 @@ import { siteDetails } from "./siteDetails";
 
 export const testimonials: ITestimonial[] = [
     {
-        name: 'John Smith',
-        role: 'CEO at Company',
-        message: `${siteDetails.siteName}'s AI-driven insights have transformed how we approach financial planning for our clients. It's an invaluable resource in the modern financial landscape.`,
-        avatar: '/images/testimonial-1.webp',
+        name: 'Mision',
+        role: '',
+        message: `En ${siteDetails.siteName} nuestra mision es proveer soluciones energéticas confiables y eficientes mediante la comercialización responsable de productos petroleros, cumpliendo con los más altos estándares de calidad, seguridad y servicio. Nuestro compromiso es ser un socio estratégico para nuestros clientes, contribuyendo al desarrollo sostenible de la industria en México.`,
+        avatar: '',
     },
     {
-        name: 'Jane Doe',
-        role: 'CTO at Startup',
-        message: `As a CTO, I'm impressed by ${siteDetails.siteName}'s robust security measures and seamless integrations. It's rare to find an app that balances user-friendliness with such advanced technology.`,
-        avatar: '/images/testimonial-2.webp',
-    },
-    {
-        name: 'Emily Johnson',
-        role: 'Product Manager',
-        message: `${siteDetails.siteName} is revolutionizing personal finance management. Its intuitive design and powerful features make it an indispensable tool for anyone serious about financial growth.`,
-        avatar: '/images/testimonial-3.webp',
+        name: 'Vision',
+        role: '',
+        message: `En ${siteDetails.siteName} nuestra vision es ser reconocidos a nivel nacional como una empresa líder en el suministro de productos petroleros al por mayor, destacando por nuestra solidez operativa, innovación constante y relaciones comerciales basadas en la confianza, el cumplimiento y la excelencia.`,
+        avatar: '',
     },
 ];
