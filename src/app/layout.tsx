a6f6afe7a29794dd062f28a03da6f2b331fa03af
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-06-10 16:09:58 -0600

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     type: 'website',
     images: [
       {
-        url: '/images/og-image.jpg',
+        url: '/images/logo.png',
         width: 1200,
         height: 675,
         alt: siteDetails.siteName,
