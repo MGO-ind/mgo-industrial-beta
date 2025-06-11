31abb438dbb14f794676379fc05436db5b880eb6
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-06-11 11:36:06 -0600

    acomodando el texto

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,6 @@ const Header: React.FC = () => {
                 <nav className="shadow-md md:shadow-none bg-white md:bg-transparent mx-auto flex justify-between items-center py-2 px-5 md:py-10">
                     {/* Logo */}
                     <Link href="/" className="flex items-center gap-2">
-                        
                         <span className="manrope text-xl font-semibold text-foreground cursor-pointer">
                             <Image src={siteDetails.centerImageSrc} alt="Logo" /> 
                             {siteDetails.siteName}
