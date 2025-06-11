f5b27db2a9b3381309873e7a8b56e298065baf9f
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-06-11 12:54:21 -0600

    info

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -22,8 +22,8 @@ export const footerDetails: {
             url: "#testimonials"
         }
     ],
-    email: 'address@yoursite.com',
-    telephone: '+1 (123) 456-7890',
+    email: ' ',
+    telephone: ' ',
     socials: {
         // github: 'https://github.com',
         // x: 'https://twitter.com/x',
