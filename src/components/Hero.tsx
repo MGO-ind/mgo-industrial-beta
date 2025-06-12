1762a56070c736516598fce7d352b156d92059e4
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-06-12 09:16:02 -0600

    prueba

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,7 +22,7 @@ const Hero: React.FC = () => {
             </div>
 
             <div className="text-center">
-                <motion.div
+                <motion.div className="flex flex-col w-full"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
