c80cccc33dd48163a749400ba3efe8553ddfd213
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-06-18 16:55:12 -0600

    imagen de whatsapp

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ import { HiOutlineXMark, HiBars3 } from 'react-icons/hi2';
 import Container from './Container';
 import { siteDetails } from '@/data/siteDetails';
 import { menuItems } from '@/data/menuItems';
-//import { FaWhatsapp } from 'react-icons/fa6';
+import { FaWhatsapp } from 'react-icons/fa6';
 
 const Header: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -41,8 +41,9 @@ const Header: React.FC = () => {
                         ))}
                         <li>
                             <Link href="https://wa.me/13311962046" target="_blank" className="text-black bg-primary hover:bg-primary-accent px-5 py-3 rounded-full transition-colors">
-                                {/*<FaWhatsapp className="min-w-fit w-5 h-5 md:w-7 md:h-7" />*/}
-                                WhatsApp
+                                {/*WhatsApp*/}
+                                <FaWhatsapp className="min-w-fit w-5 h-5 md:w-7 md:h-7" />
+                                
                             </Link>
                         </li>
                     </ul>
