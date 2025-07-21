a047824709eb9a475e068ab930adc2d912030ee2
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-21 17:28:26 -0600

    refactor: update div class for improved layout in Products component

--- src/app/productos/page.tsx
+++ src/app/productos/page.tsx
@@ -7,7 +7,7 @@ import Container from "@/components/Container";
 const Products: React.FC = () => {
   return (
     <>
-      <div className="bg-gray-100 min-h-screen">
+      <div className="margin-top-20">
         <Container className="py-10">
           <h1 className="text-3xl font-bold mb-6">Nuestros Productos</h1>
           <p className="text-lg mb-8">Descubre nuestra gama de productos diseñados para optimizar tu eficiencia energética.</p>
