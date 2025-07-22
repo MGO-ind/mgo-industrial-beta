e910a027be86425b5587acc85b40e1ce9954e609
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-22 09:51:57 -0600

    fix: update top margin for improved spacing in Products component

--- src/app/productos/page.tsx
+++ src/app/productos/page.tsx
@@ -7,7 +7,7 @@ import Container from "@/components/Container";
 const Products: React.FC = () => {
   return (
     <>
-      <div className="mt-20 mb-20">
+      <div className="mt-30 mb-20">
         <Container className="py-10">
           <h1 className="text-3xl font-bold mb-6">Nuestros Productos</h1>
           <p className="text-lg mb-8">Descubre nuestra gama de productos diseñados para optimizar tu eficiencia energética.</p>
