78e1819eaf1c869845e1ad745c1025db0190a25e
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-23 13:16:01 -0600

    feat: add Products component for multiple groups with initial layout and content

--- src/app/fichas_tecnicas/grupo_1/page.tsx
+++ src/app/fichas_tecnicas/grupo_1/page.tsx
@@ -1,8 +1,6 @@
 //import FAQ from "@/components/FAQ";
 import Container from "@/components/Container";
 
-
-
 const Products: React.FC = () => {
   return (
     <>
@@ -11,7 +9,7 @@ const Products: React.FC = () => {
           <h1 className="text-3xl font-bold mb-6">Nuestros Productos</h1>
           <p className="text-lg mb-8">Descubre nuestra gama de productos diseñados para optimizar tu eficiencia energética.</p>
    
-
+  
 
       </Container>
       </div>
