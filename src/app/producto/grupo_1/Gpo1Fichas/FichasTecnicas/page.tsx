793debe867c7345ee30158607068b8cacec91de6
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-25 09:59:22 -0600

    feat: create Products page component and enhance FichaInfoPrev layout with improved styling

---
+++ src/app/producto/grupo_1/Gpo1Fichas/FichasTecnicas/page.tsx
@@ -0,0 +1,19 @@
+//import FAQ from "@/components/FAQ";
+import Container from "@/components/Container";
+
+
+const Products: React.FC = () => {
+  return (
+    <>
+      <div className="mt-20 mb-10">
+        <Container className="py-10">
+          <h1 className="text-3xl font-bold mb-6">Nuestros Productos</h1>
+          <p className="text-lg mb-8">Descubre nuestra gama de productos diseñados para optimizar tu eficiencia energética.</p>
+          
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Products;
