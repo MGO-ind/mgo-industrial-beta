5567e7f0dc5e68763e7ee1731a591fad6f784935
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-25 12:24:50 -0600

    feat: add new DataFicha and Ficha1 components; update btnFichas link for improved navigation

--- src/app/producto/grupo_1/Gpo1Fichas/LinkFichasTecnicas.ts
+++ src/app/producto/grupo_1/Gpo1Fichas/LinkFichasTecnicas.ts
@@ -3,7 +3,7 @@ import { IBtnFichasLink } from "@/types";
 
   export const btnFichas: IBtnFichasLink[] = [
     {
-      btnHref: "/fichas/1",
+      btnHref: "/FichasTecnicas",
       titleFicha: "MGO2S6163 TDS",
       descripcionFicha: "Formulado para aceites de motores de 2 tiempos enfriados por aire, el paquete MGO2S6163 ofrece excelente detergencia, lubricación, baja generación de humo y contenido reducido de cenizas. Cumple con las especificaciones JASO M345 FB y FC, garantizando un rendimiento óptimo y limpio del motor.",
     },
