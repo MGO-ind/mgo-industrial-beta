534a2288cdd0db10fa6d8694b56abe58343b8583
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-25 17:27:46 -0600

    fix: update import path for FichaInfoPrev component

--- src/app/producto/grupo_1/page.tsx
+++ src/app/producto/grupo_1/page.tsx
@@ -1,6 +1,6 @@
 //import FAQ from "@/components/FAQ";
 import Container from "@/components/Container";
-import FichaInfoPrev from "@/components/fichaPrev";
+import FichaInfoPrev from "@/app/producto/grupo_1/Gpo1Fichas/fichaPrev";
 import { btnFichas } from "./Gpo1Fichas/LinkFichasTecnicas";
 
 const Products: React.FC = () => {
@@ -9,7 +9,6 @@ const Products: React.FC = () => {
       <div className="mt-20 mb-10">
         <Container className="py-10">
           <h1 className="text-3xl font-bold mb-6">Nuestros Productos</h1>
-
           <p className="text-lg mb-8">Descubre nuestra gama de productos diseñados para optimizar tu eficiencia energética.</p>
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
             { btnFichas.map((ficha, index) => (
