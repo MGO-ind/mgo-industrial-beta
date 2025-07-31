5336061466797bfafe7d5dc8f10909baaff71efa
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-31 11:39:52 -0600

    fix: update header and description in Products page to reflect Ficha Tecnica

--- src/app/producto/grupo_1/MGO2S6163/page.tsx
+++ src/app/producto/grupo_1/MGO2S6163/page.tsx
@@ -8,8 +8,8 @@ const Products: React.FC = () => {
     <>
       <div className="mt-20 mb-10">
         <Container className="py-10">
-          <h1 className="text-3xl font-bold mb-6">Nuestros Productos</h1>
-          <p className="text-lg mb-8">Descubre nuestra gama de productos diseñados para optimizar tu eficiencia energética.</p>
+          <h1 className="text-3xl font-bold mb-6">Ficha Tecnica</h1>
+          <p className="text-lg mb-8"></p>
 
           <Ficha1/>
           
