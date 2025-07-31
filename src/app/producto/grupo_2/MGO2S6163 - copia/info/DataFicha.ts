fd60a835e084f2a6e794927b55b9f653ca145094
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-07-31 16:49:13 -0600

    Add technical sheets for multiple products in group 1 and group 2
    - Created DataFicha and Ficha1 components for products MGO4S6164, MGO6078, MGO6079, MGO6088E, MGO6189, MGOP6340A, MGOP6400, MGOPGO4, MGOPHO5, and MGOPSL6500.
    - Implemented page components to render the technical sheets using the new Ficha1 components.
    - Each product includes a title, descriptions, and care instructions.
    - Ensured consistent styling and layout across all product pages.

---
+++ src/app/producto/grupo_2/MGO2S6163 - copia/info/DataFicha.ts
@@ -0,0 +1,10 @@
+import { IDataFicha } from "@/types";
+
+export const DataFicha: IDataFicha[] = [
+    {
+        fichaTitle: "Ficha 1",
+        fichaDescription: "Descripción de la ficha 1",
+        fichaDescription2: "Descripción adicional de la ficha 1",
+        fichaCuidados: "Cuidados de la ficha 1",
+    },
+]
