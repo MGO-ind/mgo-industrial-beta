b149e5b379b1caa50497271e67eef7e8e4c13675
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-04 17:07:27 -0600

    fix: update anchor tag class for consistent button sizing in Products component

--- src/app/producto/grupo_1/MGO2S6163/page.tsx
+++ src/app/producto/grupo_1/MGO2S6163/page.tsx
@@ -8,11 +8,11 @@ const Products: React.FC = () => {
     <>
       <div className="mt-20 mb-10">
         <Container className="py-10">
-          <a href="../grupo_1" className="inline-block w-5 mb-4 flex-col items-center justify-center">
+          <a href="../grupo_1" className="w-10 mt-10 mb-5 lg:my-20 flex-col items-center justify-center">
             <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
           </a> 
           <Ficha1/>
-          <a href="../grupo_1" className="inline-block w-5 mb-4 flex-col items-center justify-center">
+          <a href="../grupo_1" className="inline-block w-10 mb-4 flex-col items-center justify-center">
             <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
           </a>
           
