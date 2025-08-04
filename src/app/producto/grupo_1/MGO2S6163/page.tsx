d5420a7738bc8812b1afecb19ec55b031dcebf84
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-04 17:28:46 -0600

    fix: remove redundant anchor tag and restructure Regresar section for improved layout in Products component

--- src/app/producto/grupo_1/MGO2S6163/page.tsx
+++ src/app/producto/grupo_1/MGO2S6163/page.tsx
@@ -8,7 +8,7 @@ const Products: React.FC = () => {
     <>
       <div className="mt-20 mb-10">
         <Container className="py-10">
-          <section id="Regresar" className="mt-1 mb-5 lg:my-4">
+          <section className="mt-1 mb-5 lg:my-4">
             <div className="relative w-full z-30 mx-auto py-2 sm:py-2 items-center justify-center">
                 <div className="h-full flex flex-col items-center justify-center text-center px-5">
                   <a href="../grupo_1" className="absolute inset-0 -z-10 w-full ">
@@ -17,11 +17,16 @@ const Products: React.FC = () => {
                 </div>
               </div>
             </section>
-          
-          <Ficha1/>
-          <a href="../grupo_1" className="inline-block w-10 mb-4 flex-col items-center justify-center">
-            <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
-          </a>
+            <Ficha1/>
+            <section className="mt-1 mb-5 lg:my-4">
+            <div className="relative w-full z-30 mx-auto py-2 sm:py-2 items-center justify-center">
+                <div className="h-full flex flex-col items-center justify-center text-center px-5">
+                  <a href="../grupo_1" className="absolute inset-0 -z-10 w-full ">
+                    <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
+                  </a> 
+                </div>
+              </div>
+            </section>
           
         </Container>
       </div>
