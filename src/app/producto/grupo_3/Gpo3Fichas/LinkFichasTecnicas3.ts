7545bbbc475f19de2fb52a42adeaa3c7cebebbbe
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-04 17:04:55 -0600

    fix: update button hrefs in LinkFichasTecnicas components for correct product navigation

--- src/app/producto/grupo_3/Gpo3Fichas/LinkFichasTecnicas3.ts
+++ src/app/producto/grupo_3/Gpo3Fichas/LinkFichasTecnicas3.ts
@@ -3,32 +3,32 @@ import { IBtnFichasLink } from "@/types";
 
   export const btnFichas3: IBtnFichasLink[] = [
     {
-      btnHref: "/producto/grupo_1/MGO2S6163",
+      btnHref: "/producto/grupo_1/MGOC106",
       titleFicha: "MGOC106 TDS",
       descripcionFicha: "Aditivo con alto contenido de alcalinidad (400 TBN), ideal como booster para incrementar el número total de base en aceites para motores diésel, gasolina o cilindros marinos. Ofrece excelente detergencia a alta temperatura, estabilidad térmica y neutralización rápida de ácidos fuertes. Alta solubilidad en aceite y eficacia comprobada en formulaciones que requieren reforzar la resistencia a la oxidación y la acidez. ",
     },
     {
-      btnHref: "/producto/grupo_1/MGO2S6163",
+      btnHref: "/producto/grupo_1/MGOC107",
       titleFicha: "MGOC107 TDS",
       descripcionFicha: "Aditivo con alta alcalinidad (400 TBN), excelente solubilidad en aceite y gran capacidad para neutralizar ácidos fuertes. Brinda detergencia a altas temperaturas, estabilidad térmica y mejora el desempeño en aceites de motores a diésel, gasolina y otras aplicaciones industriales. Ideal como booster para incrementar el TBN en formulaciones que requieren protección prolongada contra la acidez y la oxidación.",
     },
     {
-      btnHref: "/producto/grupo_1/MGO2S6163",
+      btnHref: "/producto/grupo_1/MGOC154",
       titleFicha: "MGOC154",
       descripcionFicha: "Dispersante sin cenizas a base de poliisobutileno (Mn=1300), diseñado para mejorar la detergencia, dispersión y resistencia a la oxidación en aceites de motor. Previene la formación de depósitos y barnices, y mejora la compatibilidad con sellos de caucho fluorado. Libre de cloro, ofrece excelente desempeño en combinación con detergentes, antioxidantes e inhibidores de corrosión.",
     },
     {
-      btnHref: "/producto/grupo_1/MGO2S6163",
+      btnHref: "/producto/grupo_1/MGOC154B",
       titleFicha: "MGOC154B TDS",
       descripcionFicha: "Aditivo dispersante sin cenizas formulado a partir de poliisobutileno (Mn=1300), diseñado para evitar la formación de depósitos y barnices en pistones, mejorar la detergencia, la resistencia a la oxidación y la compatibilidad con sellos de caucho fluorado. Libre de cloro y altamente compatible con detergentes, antioxidantes e inhibidores de corrosión, es ideal para formulaciones de aceites de motor de alto desempeño.",
     },
     {
-      btnHref: "/producto/grupo_1/MGO2S6163",
+      btnHref: "/producto/grupo_1/MGOC202",
       titleFicha: "MGOC202",
       descripcionFicha: "Aditivo multifuncional tipo ZDDP (ditiofosfato de zinc) que ofrece protección anticorrosiva, antioxidante, antidesgaste y para presión extrema. Ideal para formulaciones de aceites de motor, engranajes e hidráulicos. Alta solubilidad y compatibilidad con otros aditivos. Previene el incremento de viscosidad por altas temperaturas y protege los cojinetes contra la corrosión. Dosis recomendada: 0.5% – 3.0%.",
     },
     {
-      btnHref: "/producto/grupo_1/MGO2S6163",
+      btnHref: "/producto/grupo_1/MGOC820",
       titleFicha: "MGOC820",
       descripcionFicha: "Aditivo formulado para reducir el punto de congelación y mejorar la fluidez de aceites a bajas temperaturas. Es compatible con aceites para motor e industriales, facilitando su desempeño en climas fríos o condiciones extremas. Dosis recomendada: 0.1% – 0.5%. Ideal para mejorar la operatividad de lubricantes en ambientes de baja temperatura.",
     },
--- src/app/producto/grupo_4/Gpo4Fichas/LinkFichasTecnicas4.ts
+++ src/app/producto/grupo_4/Gpo4Fichas/LinkFichasTecnicas4.ts
@@ -3,12 +3,12 @@ import { IBtnFichasLink } from "@/types";
 
   export const btnFichas4: IBtnFichasLink[] = [
     {
-      btnHref: "/producto/grupo_1/MGO2S6163",
+      btnHref: "/producto/grupo_1/MGOGRASALITIO",
       titleFicha: "MGO GRASA LITIO TDS",
       descripcionFicha: "Grasa de alto rendimiento a base de jabón de litio y aceite mineral, reforzada con aditivos EP (extrema presión), antioxidantes y antidesgaste. Ofrece excelente resistencia al agua, protección contra corrosión, oxidación y cargas pesadas, con un rango operativo desde -10°C hasta 180°C. Ideal para aplicaciones industriales, automotrices, agrícolas y mineras, en cojinetes, rodamientos, crucetas, chasis y sistemas centralizados de lubricación. Compatible con otras grasas de litio y bombeable con pistolas manuales o neumáticas.",
     },
     {
-      btnHref: "/producto/grupo_1/MGO2S6163",
+      btnHref: "/producto/grupo_1/MGOGRASAMULTILITIO",
       titleFicha: "MGO GRASA MULTILITIO TDS",
       descripcionFicha: "Grasa multipropósito a base de jabón de litio y aceites minerales refinados, con aditivos antidesgaste, antioxidantes y antiherrumbre. Ofrece excelente resistencia al agua, estabilidad mecánica y protección contra la corrosión y el desgaste, con un rango de operación desde bajo 0 °C hasta 180 °C. Recomendada para cojinetes antifricción, motores, generadores y equipos automotrices, industriales, agrícolas y mineros.",
     },
