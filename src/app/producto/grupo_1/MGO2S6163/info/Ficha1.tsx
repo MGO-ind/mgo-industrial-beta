32bdb320058c4b3f9243a040dd6a3319d757fd97
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-04 13:38:36 -0600

    fix: update specifications table in Ficha1 component for improved border styling

--- src/app/producto/grupo_1/MGO2S6163/info/Ficha1.tsx
+++ src/app/producto/grupo_1/MGO2S6163/info/Ficha1.tsx
@@ -17,7 +17,7 @@ const Ficha1 = () => {
                         <p className="mx-auto max-w-5xl md:px-5">{DataFicha[0].fichaDescription}</p>
                         <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Especificaciones</h2>
                         
-                        <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-auto md:px-5">
+                        <div className="relative overflow-x-auto shadow-md rounded-full max-w-5xl mx-auto md:px-5">
                             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
