aafcd04057a2920927e69ea0c867834297c0214c
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-04 17:22:57 -0600

    fix: update anchor tag positioning for improved layout in Regresar section of Products component

--- src/app/producto/grupo_1/MGO2S6163/page.tsx
+++ src/app/producto/grupo_1/MGO2S6163/page.tsx
@@ -11,7 +11,7 @@ const Products: React.FC = () => {
           <section id="Regresar" className="mt-1 mb-5 lg:my-4">
             <div className="relative w-full z-30 mx-auto py-2 sm:py-2">
                 <div className="w-full">
-                  <a href="../grupo_1" className="w-40 items-center justify-center">
+                  <a href="../grupo_1" className="absolute inset-0 -z-10 w-full items-center justify-center">
                     <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
                   </a> 
                 </div>
