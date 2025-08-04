0190d434cdeb7e5197b80eb830a5463144c2b80b
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-04 12:20:45 -0600

    fix: adjust heading styles and paragraph width in Ficha1 component for improved readability

--- src/app/producto/grupo_1/MGO2S6163/info/Ficha1.tsx
+++ src/app/producto/grupo_1/MGO2S6163/info/Ficha1.tsx
@@ -11,10 +11,10 @@ const Ficha1 = () => {
 
                     <div className="h-full flex flex-col items-center justify-center text-white text-center px-5">
                         <h2 className="text-2xl sm:text-3xl md:text-5xl md:leading-tight font-semibold mb-4 max-w-2xl">{DataFicha[0].fichaTitle}</h2>
-                        <h3 className="text-xl sm:text-3xl md:text-5xl md:leading-tight font-semibold mb-4">Paquete de Aditivos para Aceites de Motores de 2 Tiempos Enfriados por Aire</h3>
+                        <h3 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4">Paquete de Aditivos para Aceites de Motores de 2 Tiempos Enfriados por Aire</h3>
 
                         <h3 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Descripción</h3>
-                        <p className="mx-auto max-w-xl md:px-5">{DataFicha[0].fichaDescription}</p>
+                        <p className="mx-auto max-w-5xl md:px-5">{DataFicha[0].fichaDescription}</p>
                         <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Especificaciones</h2>
 
                         <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Envasado, Almacenamiento, Transporte</h2>
