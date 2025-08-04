2569ec7bd00a72b723624a0e9d955ab399c6ce1f
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-04 15:35:59 -0600

    fix: remove redundant class from table header in specifications section of Ficha1 component

--- src/app/producto/grupo_1/MGO2S6163/info/Ficha1.tsx
+++ src/app/producto/grupo_1/MGO2S6163/info/Ficha1.tsx
@@ -19,7 +19,7 @@ const Ficha1 = () => {
                         
                         <div className="relative overflow-x-auto shadow-md rounded-xl text-xl max-w-5xl mx-auto md:px-5">
                             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <thead className="text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
                                         <th className="px-6 py-3">ESPECIFICACIONES JASO</th>
                                         <th className="px-6 py-3">Tasa de Tratamiento (%wt)</th>
