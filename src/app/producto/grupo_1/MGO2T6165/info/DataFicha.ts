2fda57cd241332374575d4c5e72943a458ee8a6f
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-04 11:40:17 -0600

    data fichas

--- src/app/producto/grupo_1/MGO2T6165/info/DataFicha.ts
+++ src/app/producto/grupo_1/MGO2T6165/info/DataFicha.ts
@@ -3,8 +3,6 @@ import { IDataFicha } from "@/types";
 export const DataFicha: IDataFicha[] = [
     {
         fichaTitle: "Ficha 1",
-        fichaDescription: "Descripción de la ficha 1",
-        fichaDescription2: "Descripción adicional de la ficha 1",
-        fichaCuidados: "Cuidados de la ficha 1",
+        fichaDescription: "Descripción de la ficha 1"
     },
 ]
