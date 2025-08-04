c1d9f00d3a339574688d6762971c559ab4cb6ef0
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-04 16:51:10 -0600

    fix: add duplicate 'Regresar' button in Products component for improved navigation

--- src/app/producto/grupo_1/MGO2S6163/page.tsx
+++ src/app/producto/grupo_1/MGO2S6163/page.tsx
@@ -12,6 +12,9 @@ const Products: React.FC = () => {
             <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
           </a> 
           <Ficha1/>
+          <a href="../grupo_1" className="inline-block mb-4 flex-col items-center justify-center">
+            <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
+          </a>
           
         </Container>
       </div>
