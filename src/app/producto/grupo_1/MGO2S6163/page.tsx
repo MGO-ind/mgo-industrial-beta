fcb82d22de6a6473080770113abfeab45d634ec1
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-04 17:18:28 -0600

    fix: adjust layout and styling of Regresar section for improved consistency in Products component

--- src/app/producto/grupo_1/MGO2S6163/page.tsx
+++ src/app/producto/grupo_1/MGO2S6163/page.tsx
@@ -8,10 +8,10 @@ const Products: React.FC = () => {
     <>
       <div className="mt-20 mb-10">
         <Container className="py-10">
-          <section id="Regresar" className="mt-10 mb-5 lg:my-20">
-            <div className="relative h-full w-full z-10 mx-auto py-12 sm:py-20">
-                <div className="h-full w-full">
-                  <a href="../grupo_1" className="w-20 items-center justify-center">
+          <section id="Regresar" className="mt-1 mb-5 lg:my-4">
+            <div className="relative w-full z-30 mx-auto py-2 sm:py-2">
+                <div className="w-full">
+                  <a href="../grupo_1" className="w-40 items-center justify-center">
                     <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
                   </a> 
                 </div>
