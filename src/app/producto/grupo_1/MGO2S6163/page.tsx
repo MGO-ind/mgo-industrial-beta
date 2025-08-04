1684b1f5d4444e994aa39e62ab99d35822b272c9
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-04 16:50:48 -0600

    fix: update button styling and structure in Products component for consistency

--- src/app/producto/grupo_1/MGO2S6163/page.tsx
+++ src/app/producto/grupo_1/MGO2S6163/page.tsx
@@ -8,13 +8,9 @@ const Products: React.FC = () => {
     <>
       <div className="mt-20 mb-10">
         <Container className="py-10">
-          <h1 className="text-3xl font-bold mb-6">MGO2S6163</h1>
-          <a href="../grupo_1" className="inline-block mb-4 ">
-            <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
-          </a>
-            
-          
-
+          <a href="../grupo_1" className="inline-block mb-4 flex-col items-center justify-center">
+            <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
+          </a> 
           <Ficha1/>
           
         </Container>
