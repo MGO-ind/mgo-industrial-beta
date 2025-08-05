4924102dfd7bbe100f93e1cad7b9a2cb4344c55d
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-05 11:49:58 -0600

    fix: restructure layout by removing header and enhancing Regresar button placement in Products component

--- src/app/producto/grupo_1/MGO2T6165/page.tsx
+++ src/app/producto/grupo_1/MGO2T6165/page.tsx
@@ -8,11 +8,16 @@ const Products: React.FC = () => {
     <>
       <div className="mt-20 mb-10">
         <Container className="py-10">
-          <h1 className="text-3xl font-bold mb-6">Ficha Tecnica</h1>
-          <p className="text-lg mb-8"></p>
-
-          <Ficha1/>
-          
+          <section className="mt-1 mb-5 lg:my-4">
+            <div className="relative w-full z-30 mx-auto py-2 sm:py-2 items-center justify-center">
+              <div className="h-full flex flex-col items-center justify-center text-center px-5">
+                <a href="../grupo_1" className="absolute inset-0 -z-10 w-full ">
+                  <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
+                </a> 
+              </div>
+            </div>
+          </section>
+          <Ficha1/>      
         </Container>
       </div>
     </>
