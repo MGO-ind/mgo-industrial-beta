df5e1166da07c59e342410bb36e05fbf85337e13
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-05 12:26:44 -0600

    fix: update Ficha1 component with detailed product information and improved layout

--- src/app/producto/grupo_1/MGO4S6164/info/DataFicha.ts
+++ src/app/producto/grupo_1/MGO4S6164/info/DataFicha.ts
@@ -2,7 +2,7 @@ import { IDataFicha } from "@/types";
 
 export const DataFicha: IDataFicha[] = [
     {
-        fichaTitle: "Ficha 1",
-        fichaDescription: "Descripción de la ficha 1"
+        fichaTitle: "MGO4S6164 TDS",
+        fichaDescription: "El paquete de aditivos MGO4S6164 para aceites de motores de 4 tiempos contiene detergentes, dispersantes y otros aditivos de la más alta calidad. Tiene buena detergencia a alta temperatura, dispersión a baja temperatura, excelente capacidad antioxidante y anti desgaste, entre otras propiedades. Se recomienda para la formulación de aceites de motores de 4 tiempos que requieran cumplir con la función de lubricar el motor, el clutch y la transmisión al mismo tiempo. A diferentes tasas de tratamiento, se pueden formular aceites de viscosidades 10W-30, 10W-40, 15W-40, para cumplir las especificaciones API SL, SJ entre otros."
     },
 ]
