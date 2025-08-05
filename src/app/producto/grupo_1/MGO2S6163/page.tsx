10f03ed8c77cc42dff883fa56b24587f86b35058
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-05 11:45:51 -0600

    fix: refactor Regresar section layout for improved structure and consistency in Products component

--- src/app/producto/grupo_1/MGO2S6163/page.tsx
+++ src/app/producto/grupo_1/MGO2S6163/page.tsx
@@ -10,24 +10,14 @@ const Products: React.FC = () => {
         <Container className="py-10">
           <section className="mt-1 mb-5 lg:my-4">
             <div className="relative w-full z-30 mx-auto py-2 sm:py-2 items-center justify-center">
-                <div className="h-full flex flex-col items-center justify-center text-center px-5">
-                  <a href="../grupo_1" className="absolute inset-0 -z-10 w-full ">
-                    <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
-                  </a> 
-                </div>
+              <div className="h-full flex flex-col items-center justify-center text-center px-5">
+                <a href="../grupo_1" className="absolute inset-0 -z-10 w-full ">
+                  <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
+                </a> 
               </div>
-            </section>
-            <Ficha1/>
-            <section className="mt-1 mb-5 lg:my-4">
-            <div className="relative w-full z-30 mx-auto py-2 sm:py-2 items-center justify-center">
-                <div className="h-full flex flex-col items-center justify-center text-center px-5">
-                  <a href="../grupo_1" className="absolute inset-0 -z-10 w-full ">
-                    <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
-                  </a> 
-                </div>
-              </div>
-            </section>
-          
+            </div>
+          </section>
+          <Ficha1/> 
         </Container>
       </div>
     </>
