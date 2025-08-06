fe82c7790ff3191a865795b7f217155eba0b366f
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-06 10:50:25 -0600

    fix: update Regresar button to use inline-flex for improved layout and icon positioning

--- src/app/producto/grupo_1/MGO6078/page.tsx
+++ src/app/producto/grupo_1/MGO6078/page.tsx
@@ -13,7 +13,7 @@ const Products: React.FC = () => {
             <div className="relative w-full z-30 mx-auto py-2 sm:py-2 items-center justify-center">
               <div className="w-full flex flex-col items-center justify-center text-center px-5">
                 <a href="../grupo_1" className="absolute inset-0 -z-10 w-full ">
-                  <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg px-6 py-2.5 text-center me-2 mb-2 w-30">Regresar<FaArrowLeft/> </button>
+                  <button type="button" className="text-white inline-flex items-center  bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg px-6 py-2.5 text-center me-2 mb-2 w-30"><FaArrowLeft/>Regresar </button>
                 </a> 
               </div>
             </div>
