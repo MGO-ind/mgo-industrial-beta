a14f19dcec5f20bf41942469988f9ed8cfabb6e6
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-06 15:58:26 -0600

    fix: update Ficha1 and DataFicha components with new product details, specifications, and layout improvements

--- src/app/producto/grupo_1/MGOP6340A/info/DataFicha.ts
+++ src/app/producto/grupo_1/MGOP6340A/info/DataFicha.ts
@@ -2,7 +2,7 @@ import { IDataFicha } from "@/types";
 
 export const DataFicha: IDataFicha[] = [
     {
-        fichaTitle: "Ficha 1",
-        fichaDescription: "Descripción de la ficha 1"
+        fichaTitle: "MGOP6340A",
+        fichaDescription: "El paquete de aditivos MGOP6340A para aceites de motores fuera borda de 2 tiempos refrigerados por agua contiene detergentes y dispersantes de la más alta calidad. El producto brinda excelente detergencia, lubricación, baja cantidad de ceniza y poco humo. El aceite de motores fuera borda de 2 tiempos refrigerados por agua mezclado con este paquete cumple con las especificaciones NMMA TC-W3."
     },
 ]
--- src/app/producto/grupo_1/MGOP6400/info/DataFicha.ts
+++ src/app/producto/grupo_1/MGOP6400/info/DataFicha.ts
@@ -2,7 +2,7 @@ import { IDataFicha } from "@/types";
 
 export const DataFicha: IDataFicha[] = [
     {
-        fichaTitle: "Ficha 1",
-        fichaDescription: "Descripción de la ficha 1"
+        fichaTitle: "MGOP6400 CF-4/SG",
+        fichaDescription: "El paquete de aditivos MGOP6400 CF-4/SG es para aceites de motor a diesel, está compuesto por detergentes, dispersantes, antidesgaste, antioxidantes y otros aditivos de la más alta calidad. El aceite formulado para motor con este aditivo brinda una excelente dispersión del hollín, detergencia a altas temperaturas, antioxidantes, antidesgaste y anticorrosivos. Este paquete puede tener varias categorías de performance con diferentes % de tratamiento."
     },
 ]
