e84da40075b072d98dc7ec3004bddc9fe783a326
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-06 11:19:33 -0600

    fix: update Regresar button across multiple pages for improved layout and touch target size

--- src/app/producto/grupo_1/MGO2S6163/page.tsx
+++ src/app/producto/grupo_1/MGO2S6163/page.tsx
@@ -1,6 +1,7 @@
 //import FAQ from "@/components/FAQ";
 import Container from "@/components/Container";
 import Ficha1 from "./info/Ficha1";
+import { FaArrowLeft } from "react-icons/fa6";
 
 
 const Products: React.FC = () => {
@@ -10,14 +11,14 @@ const Products: React.FC = () => {
         <Container className="py-10">
           <section className="mt-1 mb-5 lg:my-4">
             <div className="relative w-full z-30 mx-auto py-2 sm:py-2 items-center justify-center">
-              <div className="h-full flex flex-col items-center justify-center text-center px-5">
+              <div className="w-full flex flex-col items-center justify-center text-center px-5">
                 <a href="../grupo_1" className="absolute inset-0 -z-10 w-full ">
-                  <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2"> ← Regresar</button>
+                  <button type="button" className="text-white inline-flex items-center  bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg px-6 py-2.5 text-center me-2 mb-2 w-30"><FaArrowLeft/>Regresar </button>
                 </a> 
               </div>
             </div>
           </section>
-          <Ficha1/> 
+          <Ficha1/>      
         </Container>
       </div>
     </>
--- src/app/producto/grupo_3/MGOC106/page.tsx
+++ src/app/producto/grupo_3/MGOC106/page.tsx
@@ -1,18 +1,23 @@
 //import FAQ from "@/components/FAQ";
 import Container from "@/components/Container";
 import Ficha1 from "./info/Ficha1";
-
+import { FaArrowLeft } from "react-icons/fa6";
 
 const Products: React.FC = () => {
   return (
     <>
       <div className="mt-20 mb-10">
-        <Container className="py-10">
-          <h1 className="text-3xl font-bold mb-6">Ficha Tecnica</h1>
-          <p className="text-lg mb-8"></p>
-
-          <Ficha1/>
-          
+       <Container className="py-10">
+          <section className="mt-1 mb-5 lg:my-4">
+            <div className="relative w-full z-30 mx-auto py-2 sm:py-2 items-center justify-center">
+              <div className="w-full flex flex-col items-center justify-center text-center px-5">
+                <a href="../grupo_1" className="absolute inset-0 -z-10 w-full ">
+                  <button type="button" className="text-white inline-flex items-center  bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg px-6 py-2.5 text-center me-2 mb-2 w-30"><FaArrowLeft/>Regresar </button>
+                </a> 
+              </div>
+            </div>
+          </section>
+          <Ficha1/>      
         </Container>
       </div>
     </>
