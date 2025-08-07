be536715f2407a31cbc697b8b69dad5cbcf47ab7
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-07 11:34:45 -0600

    fix: update Ficha1 component with enhanced product details and specifications for MGO5W40

--- src/app/producto/grupo_2/MGO5W40/info/Ficha1.tsx
+++ src/app/producto/grupo_2/MGO5W40/info/Ficha1.tsx
@@ -15,7 +15,7 @@ const Ficha1 = () => {
                         <h3 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Descripción</h3>
                         <p className="mx-auto max-w-5xl md:px-5">{DataFicha[0].fichaDescription}</p>
                         <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Cualidades</h2>
-                        <p className="mx-auto max-w-xl md:px-5">
+                        <p className="mx-auto max-w-5xl md:px-5">
                             <ul className="list-disc pl-5">
                                 <li>Recomendado para motores a gasolina y a diesel de una amplia gama de fabricantes de vehículos.</li>
                                 <li>Los ensayos demuestran una excelente limpieza del motor y economía de combustible, superior casi en un 90% al límite requerido para este grado de viscosidad. </li>
@@ -24,7 +24,7 @@ const Ficha1 = () => {
                             </ul>
                         </p>
                         <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Niveles de calidad</h2>
-                        <p className="mx-auto max-w-xl md:px-5">
+                        <p className="mx-auto max-w-5xl md:px-5">
                             <ul className="list-disc pl-5 columns-3">
                                 <li>API SN/CF, API CK-4, API CI-4</li>
                                 <li>ACEA A3/B3-04, A3/B4-04, C3</li>
@@ -67,14 +67,108 @@ const Ficha1 = () => {
                                         </td>
                                     </tr>
                                     <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
-                                        <td className="px-6 py-3"></td>
-                                        <td className="px-6 py-3"></td>
-                                        <td className="px-6 py-3"></td>
+                                        <td className="px-6 py-3">
+                                            Viscosidad @100°C, cSt
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            14.5
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            ASTM D445
+                                        </td>
                                     </tr>
                                     <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
-                                        <td className="px-6 py-3"></td>
-                                        <td className="px-6 py-3"></td>
-                                        <td className="px-6 py-3"></td>
+                                        <td className="px-6 py-3">Viscosidad @40°C, cSt</td>
+                                        <td className="px-6 py-3">88</td>
+                                        <td className="px-6 py-3">ASTM D445</td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Viscosidad @-30°C, cP
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            6600 máx.
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            ASTM D5293
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Índice de viscosidad
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            170
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            ASTM D2270
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Número Total de Bases, (mgKOH/g)
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            7.5
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            ASTM D2896
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Punto de congelación, °C
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            -39
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            ASTM D97
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Punto de Inflamabilidad (vaso abierto) (COC), °C
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            215 mín
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            ASTM D92
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Cizallamiento Inyector Bosch: <br/> Viscosidad @100°C después de cizalla, cSt
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            12.5 mín.
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            CEC L-14-93
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Volatilidad Noack, 1h a 250°C, %peso
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            12% máx.
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            CEC L.40-93
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            HTHS, viscosidad a 150°C, cP
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            &gt;3.5
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            CECC L-36-90
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -82,7 +176,7 @@ const Ficha1 = () => {
                         <section className="mt-1 mb-5 lg:my-4">
                             <div className="relative w-full z-30 mx-auto py-2 sm:py-2 items-center justify-center">
                                 <div className="h-full flex flex-col items-center justify-center text-center px-5">
-                                <a href="../grupo_1" className="absolute inset-0 -z-10 w-full ">
+                                <a href="../grupo_2" className="absolute inset-0 -z-10 w-full ">
                                     <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg p-3.5 text-center me-2 mb-2">
                                         <FaArrowLeft/>
                                     </button>
