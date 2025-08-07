5014a66327273c1159c59c89e7e464130b30587c
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-07 11:13:51 -0600

    fix: update DataFicha and Ficha1 components with detailed product information and specifications for MGO5W40

--- src/app/producto/grupo_2/MGO5W40/info/DataFicha.ts
+++ src/app/producto/grupo_2/MGO5W40/info/DataFicha.ts
@@ -2,7 +2,7 @@ import { IDataFicha } from "@/types";
 
 export const DataFicha: IDataFicha[] = [
     {
-        fichaTitle: "Ficha 1",
-        fichaDescription: "Descripción de la ficha 1"
+        fichaTitle: "MGO5W40",
+        fichaDescription: "Aceite lubricante sintético de máxima calidad especialmente diseñado para los vehículos equipados con sistemas de tratamiento de gases de escape, como los filtros de partículas DPF y FAP. Una avanzada formulación, con reducido contenido de cenizas (Mid SAPS), lo hace adecuado en los motores más desarrollados tecnológicamente, y a la vez contribuye a la conservación del medio ambiente minimizando emisiones nocivas de partículas."
     },
 ]
--- src/app/producto/grupo_2/MGO5W40/info/Ficha1.tsx
+++ src/app/producto/grupo_2/MGO5W40/info/Ficha1.tsx
@@ -1,3 +1,4 @@
+import { FaArrowLeft } from "react-icons/fa6";
 import { DataFicha } from "./DataFicha";
 
 const Ficha1 = () => {
@@ -11,27 +12,74 @@ const Ficha1 = () => {
 
                     <div className="h-full flex flex-col items-center justify-center text-white text-center px-5">
                         <h2 className="text-2xl sm:text-3xl md:text-5xl md:leading-tight font-semibold mb-4 max-w-2xl">{DataFicha[0].fichaTitle}</h2>
-                        <h3 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4">Paquete de Aditivos para Aceites de Motores de 2 Tiempos Enfriados por Aire</h3>
-
                         <h3 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Descripción</h3>
                         <p className="mx-auto max-w-5xl md:px-5">{DataFicha[0].fichaDescription}</p>
-                        <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Especificaciones</h2>
-                        
-
-                        <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Envasado, Almacenamiento, Transporte</h2>
-
+                        <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Cualidades</h2>
                         <p className="mx-auto max-w-xl md:px-5">
                             <ul className="list-disc pl-5">
-                                <li>Envasado en tambores de acero con un peso neto de 180 kg/tambor.</li>
-                                <li>La temperatura de almacenamiento no debe superar los 45 ℃ y la temperatura de carga o descarga no debe de exceder los 65 ℃.</li>
-                                <li>El producto debe ser transportado en vehículos limpios y secos, evitando la exposición a la luz solar directa.</li>
+                                <li>Recomendado para motores a gasolina y a diesel de una amplia gama de fabricantes de vehículos.</li>
+                                <li>Los ensayos demuestran una excelente limpieza del motor y economía de combustible, superior casi en un 90% al límite requerido para este grado de viscosidad. </li>
+                                <li>Probado en los test más críticos de desgaste. Los resultados permiten garantizar una mayor durabilidad del motor y menos mantenimientos. </li>
+                                <li>Su reducido contenido en cenizas lo hace necesario para la durabilidad de las nuevas tecnologías de disminución de emisiones como filtro de partículas diesel (DPF o FAP), contribuyendo por tanto en mayor medida a la conservación del medioambiente que los lubricantes convencionales.</li>
                             </ul>
                         </p>
-
-                        <div className="mt-4 flex flex-col sm:flex-row items-center sm:gap-4">
-            
-                       
+                        <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Niveles de calidad</h2>
+                        <p className="mx-auto max-w-xl md:px-5">
+                            <ul className="list-disc pl-5 columns-3">
+                                <li>API SN/CF, API CK-4, API CI-4</li>
+                                <li>ACEA A3/B3-04, A3/B4-04, C3</li>
+                                <li>MB 229.31</li>
+                                <li>FORD WSS-M2C917-A</li>
+                                <li>VW 502.00 / 505.00 / 505.01</li>
+                                <li>PORSCHE A40</li>
+                                <li>BMW LL-04</li>
+                            </ul>
+                        </p>
+                        <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Características técnicas</h2>
+                        <div className="relative overflow-x-auto shadow-md rounded-2xl text-xl max-w-5xl mx-auto mb-4 md:px-5">
+                            <table className="w-full rtl:text-right text-zinc-500 dark:text-white">
+                                <thead className="text-zinc-700 uppercase bg-gray-50 dark:bg-zinc-700 dark:text-white">
+                                    <tr>
+                                        <th className="px-6 py-3">ESPECIFICACIONES</th>
+                                        <th className="px-6 py-3">Tasa de Tratamiento (%wt)</th>
+                                        <th className="px-6 py-3">Tasa de Tratamiento (%wt)</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3"></td>
+                                        <td className="px-6 py-3"></td>
+                                        <td className="px-6 py-3"></td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3"></td>
+                                        <td className="px-6 py-3"></td>
+                                        <td className="px-6 py-3"></td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3"></td>
+                                        <td className="px-6 py-3"></td>
+                                        <td className="px-6 py-3"></td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3"></td>
+                                        <td className="px-6 py-3"></td>
+                                        <td className="px-6 py-3"></td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
+                        <section className="mt-1 mb-5 lg:my-4">
+                            <div className="relative w-full z-30 mx-auto py-2 sm:py-2 items-center justify-center">
+                                <div className="h-full flex flex-col items-center justify-center text-center px-5">
+                                <a href="../grupo_1" className="absolute inset-0 -z-10 w-full ">
+                                    <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg p-3.5 text-center me-2 mb-2">
+                                        <FaArrowLeft/>
+                                    </button>
+                                </a> 
+                                </div>
+                            </div>
+                        </section>
                     </div>
                 </div>
             </div>
