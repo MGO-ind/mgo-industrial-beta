6bc34cbbc91ea1f072a32b15d920ca4fd9ca89b5
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-07 11:58:49 -0600

    fix: update Ficha1 and DataFicha components with detailed product information and specifications for MGO GRASA CHASIS

--- src/app/producto/grupo_2/MGOGRASACHASIS/info/DataFicha.ts
+++ src/app/producto/grupo_2/MGOGRASACHASIS/info/DataFicha.ts
@@ -2,7 +2,7 @@ import { IDataFicha } from "@/types";
 
 export const DataFicha: IDataFicha[] = [
     {
-        fichaTitle: "Ficha 1",
-        fichaDescription: "Descripción de la ficha 1"
+        fichaTitle: "MGO GRASA CHASIS",
+        fichaDescription: "La MGO GRASA CHASIS, es una grasa lubricante elaborada a base de jabón de calcio y aceites minerales altamente refinados, reforzada con aditivos anti desgaste, antioxidantes y anti herrumbre, que le confieren alta resistencia al lavado por agua y excelente estabilidad mecánica. Adecuada protección contra corrosión, oxidación, desgaste y un fácil manejo tanto con pistolas manuales, como neumáticas."
     },
 ]
--- src/app/producto/grupo_2/MGOGRASACHASIS/info/Ficha1.tsx
+++ src/app/producto/grupo_2/MGOGRASACHASIS/info/Ficha1.tsx
@@ -1,3 +1,4 @@
+import { FaArrowLeft } from "react-icons/fa6";
 import { DataFicha } from "./DataFicha";
 
 const Ficha1 = () => {
@@ -11,27 +12,133 @@ const Ficha1 = () => {
 
                     <div className="h-full flex flex-col items-center justify-center text-white text-center px-5">
                         <h2 className="text-2xl sm:text-3xl md:text-5xl md:leading-tight font-semibold mb-4 max-w-2xl">{DataFicha[0].fichaTitle}</h2>
-                        <h3 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4">Paquete de Aditivos para Aceites de Motores de 2 Tiempos Enfriados por Aire</h3>
-
                         <h3 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Descripción</h3>
                         <p className="mx-auto max-w-5xl md:px-5">{DataFicha[0].fichaDescription}</p>
-                        <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Especificaciones</h2>
-                        
-
-                        <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Envasado, Almacenamiento, Transporte</h2>
-
-                        <p className="mx-auto max-w-xl md:px-5">
-                            <ul className="list-disc pl-5">
-                                <li>Envasado en tambores de acero con un peso neto de 180 kg/tambor.</li>
-                                <li>La temperatura de almacenamiento no debe superar los 45 ℃ y la temperatura de carga o descarga no debe de exceder los 65 ℃.</li>
-                                <li>El producto debe ser transportado en vehículos limpios y secos, evitando la exposición a la luz solar directa.</li>
-                            </ul>
-                        </p>
-
-                        <div className="mt-4 flex flex-col sm:flex-row items-center sm:gap-4">
-            
-                       
+                        <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Aplicación</h2>  
+                        <p className="mx-auto max-w-5xl md:px-5">Es recomendable para lubricar los estribos de sujeción, brazos de mando, muñones de la dirección y otras partes del chasis de automóviles de pasajeros, vehículos comerciales, equipos agrícolas e industriales y en general en donde no se requiera una grasa con características de servicio para altas temperaturas, ni requerimientos excepcionales anti desgaste y de extrema presión.</p>
+                        <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Presentación</h2>
+                        <p className="mx-auto max-w-xl md:px-5"> 3.5 Kg. / 16 Kg. / 180 Kg. </p>
+                        <div className="relative overflow-x-auto shadow-md rounded-2xl text-xl max-w-5xl mx-auto mb-4 md:px-5">
+                            <table className="w-full rtl:text-right text-zinc-500 dark:text-white">
+                                <thead className="text-zinc-700 uppercase bg-gray-50 dark:bg-zinc-700 dark:text-white">
+                                    <tr>
+                                        <th className="px-6 py-3"></th>
+                                        <th className="px-6 py-3">Análisis Típico</th>
+                                        <th className="px-6 py-3"></th>
+                                    </tr>
+                                    <tr>
+                                        <th className="px-6 py-3">
+                                            Propiedad
+                                        </th>
+                                        <th className="px-6 py-3">
+                                            Método ASTM
+                                        </th>
+                                        <th className="px-6 py-3">
+                                            Resultados
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Grado NLGI
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            D-217
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            2
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Color
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            Visual
+                                        </td>
+                                        <td className="px-6 py-3">
+                                             Roja, Azul o Ámbar
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Penetración a 25 °C
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            D-217
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            270 – 275
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Penetración a 60 golpes a 25 ° C
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            D-217
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            270 - 275
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Penetración a 10,000 golpes a 25 °C
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            D-217
+                                        </td>
+                                        <td className="px-6 py-3">-</td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Estabilidad Mecánica
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            D-217
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            5 % máx.
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Resistencia al lavado por agua a 77 °C
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            D-1264
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            2 % máx.
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">
+                                            Temperatura de goteo °C
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            D-2265
+                                        </td>
+                                        <td className="px-6 py-3">
+                                            95 mín.
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
+                        <section className="mt-1 mb-5 lg:my-4">
+                            <div className="relative w-full z-30 mx-auto py-2 sm:py-2 items-center justify-center">
+                                <div className="h-full flex flex-col items-center justify-center text-center px-5">
+                                <a href="../grupo_2" className="absolute inset-0 -z-10 w-full ">
+                                    <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full text-lg p-3.5 text-center me-2 mb-2">
+                                        <FaArrowLeft/>
+                                    </button>
+                                </a> 
+                                </div>
+                            </div>
+                        </section>
                     </div>
                 </div>
             </div>
