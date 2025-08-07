4c24b7b32a392d0e16e98b17d5c3257b3bba98ee
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-07 16:15:15 -0600

    fix: update WhatsApp link styling for improved visibility and user experience

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,7 +40,7 @@ const Header: React.FC = () => {
                             </li>
                         ))}
                         <li>
-                            <Link href="https://wa.me/13311962046" target="_blank" className="text-black bg-primary hover:bg-primary-accent px-5 py-3 rounded-full transition-colors">
+                            <Link href="https://wa.me/13311962046" target="_blank" className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">
                                 {/*WhatsApp <FaWhatsapp />*/} 
                                 WhatsApp
                                 
