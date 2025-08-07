4135e4fac7dbd7e168fc97e49bcf0133efcafcd7
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-08-07 13:38:55 -0600

    fix: update DataFicha and Ficha1 components with detailed product information and specifications for MGOC107

--- src/app/producto/grupo_3/MGOC107/info/DataFicha.ts
+++ src/app/producto/grupo_3/MGOC107/info/DataFicha.ts
@@ -2,7 +2,7 @@ import { IDataFicha } from "@/types";
 
 export const DataFicha: IDataFicha[] = [
     {
-        fichaTitle: "Ficha 1",
-        fichaDescription: "Descripción de la ficha 1"
+        fichaTitle: "MGOC107 TDS",
+        fichaDescription: "El producto MCOC107 tiene una excelente propiedad de alcalinidad (400 TBN), buena solubilidad en aceite y capacidad para neutralizar inmediatamente ácidos fuertes sean orgánicos o inorgánicos, mientras que también tiene una excelente detergencia a temperaturas altas y estabilidad térmica.  Es usado comúnmente como un aditivo para mezclar en aceite, como booster para incrementar el TBN en aceites para motores a diesel, gasolina, etc."
     },
 ]
--- src/app/producto/grupo_3/MGOC107/info/Ficha1.tsx
+++ src/app/producto/grupo_3/MGOC107/info/Ficha1.tsx
@@ -12,20 +12,69 @@ const Ficha1 = () => {
 
                     <div className="h-full flex flex-col items-center justify-center text-white text-center px-5">
                         <h2 className="text-2xl sm:text-3xl md:text-5xl md:leading-tight font-semibold mb-4 max-w-2xl">{DataFicha[0].fichaTitle}</h2>
-                        <h3 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4">Paquete de Aditivos para Aceites de Motores de 2 Tiempos Enfriados por Aire</h3>
+                        <h3 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4">Detergente Sulfonato de Magnesio Sintético(Booster TBN)</h3>
 
                         <h3 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Descripción</h3>
                         <p className="mx-auto max-w-5xl md:px-5">{DataFicha[0].fichaDescription}</p>
                         <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Especificaciones</h2>
-                        
+                        <div className="relative overflow-x-auto shadow-md rounded-2xl text-xl max-w-5xl mx-auto mb-4 md:px-5">
+                            <table className="w-full rtl:text-right text-zinc-500 dark:text-white">
+                                <thead className="text-zinc-700 uppercase bg-gray-50 dark:bg-zinc-700 dark:text-white">
+                                    <tr>
+                                        <th className="px-6 py-3">Pruebas</th>
+                                        <th className="px-6 py-3">Métodos ASTM</th>
+                                        <th className="px-6 py-3">Especificaciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">Punto de Inflamación (COC), ℃</td>
+                                        <td className="px-6 py-3">D92</td>
+                                        <td className="px-6 py-3">≥ 180</td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">Viscosidad Cinemática @100 ℃, mm2/s</td>
+                                        <td className="px-6 py-3">D445</td>
+                                        <td className="px-6 py-3">80 - 150</td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">Densidad @20 ℃, kg/m3</td>
+                                        <td className="px-6 py-3">D4052</td>
+                                        <td className="px-6 py-3">1050 - 1150</td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">Número Total de Base, (mg KOH/g)</td>
+                                        <td className="px-6 py-3">D2896</td>
+                                        <td className="px-6 py-3">395 - 435</td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">Contenido de Mg, % peso</td>
+                                        <td className="px-6 py-3">D4951</td>
+                                        <td className="px-6 py-3">8.5 - 10.0</td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">Contenido de Agua, %</td>
+                                        <td className="px-6 py-3">D95</td>
+                                        <td className="px-6 py-3">≤ 0.2</td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-300 border-zinc-500">
+                                        <td className="px-6 py-3">Sedimentos, % peso</td>
+                                        <td className="px-6 py-3"></td>
+                                        <td className="px-6 py-3">≤ 0.1</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
 
                         <h2 className="text-xl sm:text-1xl md:text-2xl md:leading-tight font-semibold mb-4 max-w-5xl">Envasado, Almacenamiento, Transporte</h2>
 
                         <p className="mx-auto max-w-xl md:px-5">
                             <ul className="list-disc pl-5">
-                                <li>Envasado en tambores de acero con un peso neto de 180 kg/tambor.</li>
-                                <li>La temperatura de almacenamiento no debe superar los 45 ℃ y la temperatura de carga o descarga no debe de exceder los 65 ℃.</li>
-                                <li>El producto debe ser transportado en vehículos limpios y secos, evitando la exposición a la luz solar directa.</li>
+                                <li>Envasado en tambores de acero de 200 Lt..</li>
+                                <li>LLa temperatura de almacenamiento no debe exceder los 45 °C.</li>
+                                <li>El producto no es flamable, ni explosivo, ni corrosivo, y es igual a los demás productos derivados del petróleo en términos de seguridad, protección ambiental, uso y otros aspectos. No necesita protección especial.</li>
+                                <li>El producto almacenado cuenta con una vida útil de 2 años y debe evitarse el contacto con antioxidantes fuertes al momento de usarse.</li>
+                                <li>En caso de entrar en contacto con la piel, lávese con agua y detergente.</li>
                             </ul>
                         </p>
 
